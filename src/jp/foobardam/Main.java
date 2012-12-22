f36cdf4fc1f45a773b02c946d7d1191cda6c78b0
foobardam <foobardam@gmail.com>
2012-12-23 03:41:01 +0900

    BeanUtilを利用してプロパティを表示した

--- src/jp/foobardam/Main.java
+++ src/jp/foobardam/Main.java
@@ -1,7 +1,25 @@
 package jp.foobardam;
 
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.InvocationTargetException;
+
+import org.apache.commons.beanutils.PropertyUtils;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println(new Person("太郎", 28, "090-1234-5678"));
+		Person p = new Person("太郎", 28, "090-1234-5678");
+		for(PropertyDescriptor pd : PropertyUtils.getPropertyDescriptors(p)){
+			try{
+				String propName = pd.getName();
+				Object prop = PropertyUtils.getProperty(p, propName);
+				System.out.format("プロパティ(%s) => %s%n",  propName, prop);
+			} catch (IllegalAccessException e){
+				e.printStackTrace();
+			} catch (InvocationTargetException e){
+				e.printStackTrace();
+			} catch (NoSuchMethodException e){
+				e.printStackTrace();
+			}
+		}
 	}
 }
